f17cf28bdc9717dbdca0ddca08a97542a50d3a3b
Pratik Dahekar <pratikdahekar7@gmail.com>
2024-10-30 19:02:08 +0530

    Navbar Added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,25 @@
+import logo from "../assets/kevinRushLogo.png";
+import { FaLinkedin, FaStackOverflow } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      {/* Logo */}
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} className="mx-2 w-10" alt="logo" />
+      </div>
+
+      {/* Social Icons */}
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaStackOverflow />
+        <FaInstagram />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
